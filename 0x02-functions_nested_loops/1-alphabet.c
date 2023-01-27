7fef6c7927b09e9a91e651ce039489082c7dc354
chris <christopherdiggs1@gmail.com>
2023-01-27 12:14:19 -0800

    abc

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,11 @@
 #include"main.h"
 void print_alphabet(void)
 {
-char ab = 'a';
-while (ab <= 'z')
-{
-_putchar(ab);
-ab++;
-}
-return (0);
+	char ab = 'a';
+	while (ab <= 'z')
+	{
+		_putchar(ab);
+		ab++;
+	}
+	return (0);
 }
