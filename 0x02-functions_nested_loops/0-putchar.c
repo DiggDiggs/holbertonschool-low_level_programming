61c5f2036a8695bcc0582e695e1d82d974e6fb29
chris <christopherdiggs1@gmail.com>
2023-01-27 10:11:22 -0800

    puchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,16 +2,16 @@
  * main - jbghjdchdfsfjggj
  * Return: return 0
  */
-#include"main.h"
+#include <stdio.h>
 int main(void)
 {
-int pp[9] = { '_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\n' };
-int i = 0;
-while (i < 9)
-{
-	putchar(pp[i]);
-	i++;
-}
+	int pp[9] = { '_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\n' };
+	int i = 0;
+	while(i < 9)
+	{
+		putchar(pp[i]);
+		i++;
+	}
 
 	return (0);
 }
