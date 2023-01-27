82b83ee5b1a823f04f0b99e5f8dd309a6895e02f
chris <christopherdiggs1@gmail.com>
2023-01-27 13:13:58 -0800

    abc10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,14 @@
-/**
- * print_alphabet - does something
- * Return: void
- */
-#include"main.h"
-void print_alphabet(void)
-{
-	char ab;
+#include "main.h"
 
-	for (ab = 'a'; ab <= 'z'; ab++)
-	{
-		_putchar((ab % 10) + '0');
-	}
+/**
+ * print_numbers - Prints the numbers from 0-9.
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+
 	_putchar('\n');
 }
