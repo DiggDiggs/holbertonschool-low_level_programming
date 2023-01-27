61c74bf125014db5570a3fb34a56ba8e42be4ca5
chris <christopherdiggs1@gmail.com>
2023-01-27 08:53:32 -0800

    ptchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+/*
+ * File: 0-holberton.c
+ * Auth: Chris D.
+ */
+
+#include "_putchar"
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf('_putchar'\n);
+	return (0);
+}
