092fed7e89d7f1bb60411d7e6f372d7f4816892c
chris <christopherdiggs1@gmail.com>
2023-01-27 21:47:50 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - uses islower
+ */
+#include "main.h"
+int _islower(int c)
+
+{
+	if ( _islower(c)  < 0)
+	{
+		_putchar('1');
+	}
+	else
+		{
+			_putchar('0');
+		}	
+	return (0);
+}
