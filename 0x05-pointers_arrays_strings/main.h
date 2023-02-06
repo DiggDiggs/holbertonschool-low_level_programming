13f3ce09f35ca7684566770c824f04a815700ce5
chris <christopherdiggs1@gmail.com>
2023-02-05 16:51:02 -0800

    rev

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -15,7 +15,7 @@ void swap_int(int *a, int *b);
 size_t _strlen(const char *str);
 void _puts(char *str);
 void print_rev(char *s);
-void rev_string(char *s);
+void reverse_array(int *a, int n);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
