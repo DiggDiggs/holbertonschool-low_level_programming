35dd612343028267616ed5ed901598f9a116eef9
chris <christopherdiggs1@gmail.com>
2023-02-16 13:55:08 -0800

    commit

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of a number
+ * @i: the value to test
+ *
+ * Return: the absolute value of 1
+ */
+
+int _abs(int i)
+{
+return (i < 0 ? -i : i);
+}
