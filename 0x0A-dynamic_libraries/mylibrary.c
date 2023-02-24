abc0a6be2ff4c34f9d93ea1dcf378d295c229925
chris <christopherdiggs1@gmail.com>
2023-02-24 13:30:17 -0800

    commit

---
+++ 0x0A-dynamic_libraries/mylibrary.c
@@ -0,0 +1,82 @@
+#include <stddef.h>
+
+int _putchar(char c) {
+    // Your implementation of _putchar
+}
+
+int _islower(int c) {
+    // Your implementation of _islower
+}
+
+int _isalpha(int c) {
+    // Your implementation of _isalpha
+}
+
+int _abs(int n) {
+    // Your implementation of _abs
+}
+
+int _isupper(int c) {
+    // Your implementation of _isupper
+}
+
+int _isdigit(int c) {
+    // Your implementation of _isdigit
+}
+
+size_t _strlen(const char *s) {
+    // Your implementation of _strlen
+}
+
+void _puts(const char *s) {
+    // Your implementation of _puts
+}
+
+char *_strcpy(char *dest, const char *src) {
+    // Your implementation of _strcpy
+}
+
+int _atoi(const char *s) {
+    // Your implementation of _atoi
+}
+
+char *_strcat(char *dest, const char *src) {
+    // Your implementation of _strcat
+}
+
+char *_strncat(char *dest, const char *src, size_t n) {
+    // Your implementation of _strncat
+}
+
+char *_strncpy(char *dest, const char *src, size_t n) {
+    // Your implementation of _strncpy
+}
+
+int _strcmp(const char *s1, const char *s2) {
+    // Your implementation of _strcmp
+}
+
+void *_memset(void *s, int c, size_t n) {
+    // Your implementation of _memset
+}
+
+void *_memcpy(void *dest, const void *src, size_t n) {
+    // Your implementation of _memcpy
+}
+
+char *_strchr(const char *s, int c) {
+    // Your implementation of _strchr
+}
+
+size_t _strspn(const char *s, const char *accept) {
+    // Your implementation of _strspn
+}
+
+char *_strpbrk(const char *s, const char *accept) {
+    // Your implementation of _strpbrk
+}
+
+char *_strstr(const char *haystack, const char *needle) {
+    // Your implementation of _strstr
+}
+
